b274ceeb4426b34143c8e801cd5280ddfdc2c21e
ankurs18 <ankursethi1994@gmail.com>
2020-04-23 04:15:53 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Botui from 'botui-react';
 
-function App() {
-  return (
+
+
+class App extends Component {
+  componentDidMount() {
+    this.botui.message.bot({
+        content: "Hello World",
+        delay: 1000
+    });
+    this.botui.action.text({
+        action: {
+            placeholder: "Enter your text here"
+        }
+    });
+  }
+
+  render() {return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Botui ref={ cmp => this.botui = cmp } />
       </header>
     </div>
   );
+}
+
 }
 
 export default App;
